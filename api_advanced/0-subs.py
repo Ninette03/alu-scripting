db03b2aa539422795a9a39ea8360305ebf37ec99
Ninette03 <n.agatesi@alustudent.com>
2024-07-05 08:34:28 -0700

    New files

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,13 +8,14 @@ import sys
 
 
 def number_of_subscribers(subreddit):
-     """Return the total number of subscribers on a given subreddit."""
-     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    """get all subscribers"""
+    if len(sys.argv) < 2:
         return 0
+    else:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        headers = {"User-Agent": "Mozilla/5.0"}
+        result = requests.get(url, headers=headers, allow_redirects=False)
+        if result.status_code != 200:
+            return 0
+        body = json.loads(result.text)
+        return body["data"]["subscribers"]
